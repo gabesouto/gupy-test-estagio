f39af00ddd3a2b9b264be863c8aea640143851f0
gabesouto <soutogabriel04@gmail.com>
2024-08-30 11:52:35 -0300

    Find sum exercise

---
+++ src/ex3.java
@@ -0,0 +1,16 @@
+public class ex3 {
+
+  public static void main(String[] args) {
+    int INDICE = 12, SOMA = 0, K = 1;
+
+    while (K < INDICE) {
+      K = K + 1;
+      SOMA = SOMA + K;
+    }
+
+    System.out.println(SOMA);
+
+    //VALOR FINAL: 77
+
+  }
+}
